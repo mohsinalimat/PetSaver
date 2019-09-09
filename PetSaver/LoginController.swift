46e7d702fa2a02bc01b42a010f0ee55b11a42527
Boaz Froog <boaz.froog@gmail.com>
2019-09-09 20:24:36 +0300

    Added Firebase pod

--- PetSaver/LoginController.swift
+++ PetSaver/LoginController.swift
@@ -92,6 +92,8 @@ class LoginController: UIViewController {
         navigationController?.isNavigationBarHidden = true
         view.backgroundColor = .white
         
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 10, paddingRight: 0, width: 0, height: 50)
         
         setupInputFields()
     }
