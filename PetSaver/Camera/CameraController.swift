d08c3b9e39432ec05da957bc76098201fd35fef3
Boaz Froog <boaz.froog@gmail.com>
2019-09-16 18:40:51 +0300

    capturing photo

--- PetSaver/Camera/CameraController.swift
+++ PetSaver/Camera/CameraController.swift
@@ -9,12 +9,71 @@
 import UIKit
 import AVFoundation
 
-class CameraController: UIViewController {
+class CameraController: UIViewController, AVCapturePhotoCaptureDelegate {
+   
+    let capturePhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "capture_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleCapturePhoto), for: .touchUpInside)
+        return button
+    }()
+    
+    let dismissButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "right_arrow_shadow").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc func handleDismiss() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupCaptureSession()
+        
+        setupHUD()
+        
     }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    fileprivate func setupHUD() {
+        view.addSubview(capturePhotoButton)
+        capturePhotoButton.anchor(top: nil, left: nil, bottom: view.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 24, paddingRight: 0, width: 80, height: 80)
+        capturePhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        view.addSubview(dismissButton)
+        dismissButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: view.rightAnchor, paddingTop: 12, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 50, height: 50)
+    }
+    
+    @objc func handleCapturePhoto() {
+        let settings = AVCapturePhotoSettings()
+        
+        guard let previewFormatType = settings.availablePreviewPhotoPixelFormatTypes.first else { return }
+        
+        settings.previewPhotoFormat = [kCVPixelBufferPixelFormatTypeKey as String: previewFormatType]
+        
+        output.capturePhoto(with: settings, delegate: self)
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let imageData = photo.fileDataRepresentation() else { return }
+        
+        let previewImage = UIImage(data: imageData)
+        let previewImageView = UIImageView(image: previewImage)
+        
+        view.addSubview(previewImageView)
+        previewImageView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+    }
+    
+    let output = AVCapturePhotoOutput()
     
     fileprivate func setupCaptureSession() {
         let captureSession = AVCaptureSession()
@@ -28,8 +87,7 @@ class CameraController: UIViewController {
         } catch let err {
             print("Could not setup camera input: ", err)
         }
-        
-        let output = AVCapturePhotoOutput()
+                
         if captureSession.canAddOutput(output) {
             captureSession.addOutput(output)
         }
