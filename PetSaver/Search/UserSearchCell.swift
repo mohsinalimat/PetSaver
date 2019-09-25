6d4c39caf72b9b39acbba7d98c3dd0ab2d0ff5bb
Boaz Froog <boaz.froog@gmail.com>
2019-09-25 18:30:36 +0300

    user search functionality

--- PetSaver/Search/UserSearchCell.swift
+++ PetSaver/Search/UserSearchCell.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class UserSearchCell: UICollectionViewCell {
     
+    var user: User? {
+        didSet {
+            usernameLabel.text = user?.username
+            
+            guard let profileImageUrl = user?.profileImageUrl else { return }
+            
+            profileImageView.loadImage(urlString: profileImageUrl)
+        }
+    }
+    
     let profileImageView: CustomImageView = {
         let iv = CustomImageView()
         iv.backgroundColor = .red
--- PetSaver/Search/UserSearchController.swift
+++ PetSaver/Search/UserSearchController.swift
@@ -7,16 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
-class UserSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class UserSearchController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
    
-    let searchBar: UISearchBar = {
+    lazy var searchBar: UISearchBar = {
         let sb = UISearchBar()
         sb.placeholder = "Enter username"
         sb.barTintColor = .gray
         UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = UIColor.rgb(red: 230, green: 230, blue: 230)
+        sb.delegate = self
         return sb
     }()
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchText.isEmpty {
+            filteredUsers = users
+        } else {
+            filteredUsers = self.users.filter { (user) -> Bool in
+                return user.username.lowercased().contains(searchText.lowercased())
+            }
+        }
+        
+        collectionView.reloadData()
+        
+    }
     
     let cellId = "cellId"
     
@@ -33,15 +49,50 @@ class UserSearchController: UICollectionViewController, UICollectionViewDelegate
         collectionView.register(UserSearchCell.self, forCellWithReuseIdentifier: cellId)
         
         collectionView.alwaysBounceVertical = true
+        
+        fetchUsers()
+    }
+    
+    var filteredUsers = [User]()
+    var users = [User]()
+    fileprivate func fetchUsers() {
+        
+        let ref = Database.database().reference().child("users")
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            
+            guard let dictionaries = snapshot.value as? [String: Any] else { return }
+            
+            dictionaries.forEach { (key, value) in
+                guard let userDictionary = value as? [String: Any] else { return }
+                
+                let user = User(uid: key, dictionary: userDictionary)
+                self.users.append(user)
+            }
+            
+            self.users.sort { (u1, u2) -> Bool in
+                
+                return u1.username.compare(u2.username) == .orderedAscending
+                
+            }
+            
+            self.filteredUsers = self.users
+            self.collectionView.reloadData()
+            
+        }) { (err) in
+            print("Failed to fetch users for search: ", err)
+        }
+        
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return filteredUsers.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! UserSearchCell
          
+        cell.user = filteredUsers[indexPath.item]
+        
         return cell
     }
     
